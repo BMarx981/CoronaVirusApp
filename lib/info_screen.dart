1a59095c858201dc8dad40d03c9c8aeb7834d1b9
BMarx981 <b.marx981@gmail.com>
2020-04-02 14:43:42 -0400

    "Add link to NYTimes data set add url launcher"

--- lib/info_screen.dart
+++ lib/info_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class InfoScreen extends StatelessWidget {
   @override
@@ -13,15 +14,46 @@ class InfoScreen extends StatelessWidget {
           15,
         ),
       ),
-      body: Container(
-        child: Column(
-          children: <Widget>[
-            Text(
-              'Data provided by the NYTimes',
-            ),
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'Data provided by the NYTimes',
+                style: TextStyle(fontSize: 18),
+              ),
+              InkWell(
+                child: Text(
+                  'Link to data',
+                  style: TextStyle(
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                    fontSize: 18.0,
+                  ),
+                ),
+                onTap: () {
+                  _launchInBrowser(
+                      'https://www.nytimes.com/interactive/2020/us/coronavirus-us-cases.html');
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Future<void> _launchInBrowser(String url) async {
+    if (await canLaunch(url)) {
+      await launch(
+        url,
+        forceSafariVC: false,
+        forceWebView: false,
+      );
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
